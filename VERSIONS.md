cadbaf849ce0a096103f01eb0601c25e0d0a3f20
Pawel Osiczko <p.osiczko@tetrapyloctomy.org>
2023-02-08 22:08:54 -0700

    Added project skeleton
    Generated with [Gemsmith](https://www.alchemists.io/projects/gemsmith) 19.4.0.

---
+++ VERSIONS.md
@@ -0,0 +1,5 @@
+# Versions
+
+## 0.0.0 (2023-02-08)
+
+- Added initial implementation.
---
+++ lib/active911_api.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "zeitwerk"
+
+Zeitwerk::Loader.for_gem.setup
+
+# Main namespace.
+module Active911Api
+end
