4c710160fe64fdb3cc223b063af5c82e3fd266e3
Pawel Osiczko <p.osiczko@tetrapyloctomy.org>
2023-02-15 07:00:38 -0700

    Fixed the agency get call

--- lib/active911/api/resources/agency_resource.rb
+++ lib/active911/api/resources/agency_resource.rb
@@ -2,7 +2,7 @@ module Active911
   module API
     class AgencyResource < Resource
       def get
-        result = get_request("/")
+        result = get_request("")
         # API unfortunately returns HTML instead of JSON encoding
         # even though the content-type is set to application/json
         # so we have to parse the HTML and extract the JSON
