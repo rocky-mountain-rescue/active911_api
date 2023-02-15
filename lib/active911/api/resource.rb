5e71cb6a02caf220139c0f644a2b446318bbb8a1
Pawel Osiczko <p.osiczko@tetrapyloctomy.org>
2023-02-15 06:46:11 -0700

    Added model, request, resource

---
+++ lib/active911/api/resource.rb
@@ -0,0 +1,62 @@
+module Active911
+  module API
+    class Resource
+      attr_reader :client
+
+      def initialize(client)
+        client.get_access_token
+        @client = client
+      end
+
+      def get_request(url, params: {}, headers: {})
+        handle_response client.connection.get(url, params, default_headers.merge(headers))
+      end
+
+      def post_request(url, body:, headers: {})
+        handle_response client.connection.post(url, body, default_headers.merge(headers))
+      end
+
+      def put_request(url, body:, headers: {})
+        handle_response client.connection.put(url, body, default_headers.merge(headers))
+      end
+
+      def patch_request(url, body: {}, headers: {})
+        handle_response client.connection.patch(url, body, default_headers.merge(headers))
+      end
+
+      def delete(url, params: {}, headers: {})
+        handle_response client.connection.delete(url, params, default_headers.merge(headers))
+      end
+
+      def default_headers
+        {
+          Authorization: "Bearer #{client.api_key}"
+        }
+      end
+
+      def handle_response(response)
+        case response.status
+          when 400
+            raise Error, response.body
+          when 401
+            raise Error, response.body
+          when 403
+            raise Error, response.body
+          when 404
+            raise Error, response.body
+          when 429
+            raise Error, response.body
+          when 500
+            raise Error, response.body
+        end
+        response
+      end
+
+      private
+
+      def error_meta_info(body)
+        body.dig("meta").to_s
+      end
+    end
+  end
+end
